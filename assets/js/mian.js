febe81b7ce0b97d6bae83067fc3565ad9f96af3c
SAIFUL ISLAM <web.saifulislam@gmail.com>
2023-11-16 21:34:03 +0600

    js : updated

--- assets/js/mian.js
+++ assets/js/mian.js
@@ -174,12 +174,6 @@
         }
 
         /*----------------------------------------------
-            include html
-        ----------------------------------------------*/
-        $(function(){$("*[include-html]").each(function(){var t=$(this).attr("include-html"),u=this.id;jQuery.ajax({url:t,success:function(t){$("#"+u).html(t)},error:function(n,c,i){var s=n.status+": "+n.statusText;$("#"+u).html(t+"-"+s)}})})});
-
-
-        /*----------------------------------------------
             Progress
         ----------------------------------------------*/
         $('.bar-1').rProgressbar({
